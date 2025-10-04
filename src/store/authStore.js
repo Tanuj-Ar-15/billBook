3c0df772d9494e3fc77fc018871b267e697c386e
Tanuj <tanujarora1515@gmail.com>
2025-10-04 17:17:22 +0530

    Code merged and set up frontend

---
+++ src/store/authStore.js
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+
+// ✅ Correct Zustand store syntax
+const useAuthStore = create(( get, set) => ({
+  user: {name : "uday"} ,            
+  token: null,             
+  isAuthenticated: false,  
+
+}));
+
+export default useAuthStore;
